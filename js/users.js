0c6c782a55fbbeff3befaa92fd49b73094eff024
Temirlan Isabekov <temirlanisabekov@MacBook-Pro-Temirlan.local>
2022-03-07 00:06:59 +0600

    Fix

--- js/users.js
+++ js/users.js
@@ -1,16 +1,23 @@
-(async function() {
+(async function () {
   const myHeaders = new Headers({
     'Authorization': 'Token ' + localStorage.getItem('CognitoIdentityServiceProvider.2ld7vl8f5uknh7n37ac5ohve6j.temirlan.i-at-mvpngn.com.accessToken'),
     'Content-Type': 'application/x-www-form-urlencoded'
   });
 
-  const response = await fetch('https://main.d3o1uetdft0902.amplifyapp.com/users', {
-    method: 'GET',
-    headers: myHeaders,
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-  });
-  const result = await response.json();
-  console.log(result);
+  try {
+    const response = await fetch('https://main.d3o1uetdft0902.amplifyapp.com/users', {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+    });
+    console.log(response);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error)
+  }
+
+
 })()
