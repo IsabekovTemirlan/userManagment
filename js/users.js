16f3ffa536039ded1dc8263ab8f7deff05a3a51f
Temirlan Isabekov <temirlanisabekov@MacBook-Pro-Temirlan.local>
2022-03-07 23:04:32 +0600

    Finish: delete handler

--- js/users.js
+++ js/users.js
@@ -1,13 +1,14 @@
 (async function () {
   try {
     const token = await AppContext.authToken
+    const url = _config.api.invokeUrl + '/users';
 
-    const response = await fetch(_config.api.invokeUrl + '/users', {
+    const response = await fetch(url, {
       method: 'GET',
       headers: {
         'Authorization': token,
       },
-      cache: 'no-cache',
+      cache: 'no-cache'
     });
     const result = await response.json();
     if (result && result.body) {
@@ -23,7 +24,7 @@
 
 function setUsers(data) {
   const container = document.getElementById('usersContainer');
-  container.insertAdjacentHTML('afterbegin', data.map(mapData).filter(Boolean).join(''));
+  container.insertAdjacentHTML('afterbegin', data.sort(compareDates).map(mapData).filter(Boolean).join(''));
   const btns = document.querySelectorAll('#deleteBtn');
   btns.forEach(btn => {
     const id = btn.dataset.id;
@@ -44,11 +45,37 @@ function mapData(item, idx) {
     <td>${item?.email || '-'}</td>
     <td>${item?.phone || '-'}</td>
     <td>${item?.age || '-'}</td>
+    <td>${item?.created ? new Date(item.created).toLocaleDateString() : '-'}</td>
     <td id="deleteBtn" data-id="${item.UserId}"><button>delete</button></td>
     </tr>`;
   }
 }
 
-function deleteUser(id) {
-  console.log(id)
+async function deleteUser(id) {
+  try {
+    const token = await AppContext.authToken
+    const url = _config.api.invokeUrl + '/users';
+
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': token
+      },
+      body: JSON.stringify({id})
+    });
+    if (response) {
+      console.log(response);
+      const data = await response.json();
+      if (data) {
+        location.reload();
+      }
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function compareDates(a, b) {
+  if (a.created && b.created) return new Date(b.created) - new Date(a.created);
+  else return 0
 }
