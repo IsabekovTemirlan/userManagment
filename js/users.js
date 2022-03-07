379809b6e90752467fb3357fe552c265169ba582
Temirlan Isabekov <temirlanisabekov@MacBook-Pro-Temirlan.local>
2022-03-07 10:58:16 +0600

    Fix

--- js/users.js
+++ js/users.js
@@ -1,18 +1,13 @@
 (async function () {
   try {
     const token = await AppContext.authToken
-    // if (!token) window.location.href = '/signin.html';
-
-    const myHeaders = new Headers({
-      'Authorization': token
-    });
 
     const response = await fetch(_config.api.invokeUrl + '/users', {
       method: 'GET',
-      headers: myHeaders,
-      mode: 'no-cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+          'Authorization': token,
+      },
+      cache: 'no-cache',
     });
     console.log(response);
     const result = await response.json();
